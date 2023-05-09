4697f7a57259d02e10a74b140cb75ff1ad136278
joseph <joseph.ketterer@gmail.com>
2023-05-09 21:39:06 +0100

    first commit

---
+++ oop/src/Car.java
@@ -0,0 +1,60 @@
+public class Car {
+
+    private String make = "Tesla";
+    private String model = "model x";
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public void setConvertible(boolean convertible) {
+        this.convertible = convertible;
+    }
+
+    private String color = "grey";
+    private int doors = 4;
+    private boolean convertible  = true;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+
+
+    public void describeCar() {
+
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+}
